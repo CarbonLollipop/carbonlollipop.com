f1a7138300be9523612dfc5291f4760b92ac2050
CarbonLollipop <93494083+CarbonLollipop@users.noreply.github.com>
2022-12-15 20:17:09 -0800

    MESSY MESSY MESS MESS

--- index.php
+++ index.php
@@ -6,16 +6,42 @@
 			<link rel="stylesheet" type="text/css" href="/style.css">
 			<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 			<script src="main.js" defer></script>
-			<meta name="title" content="if adam was here right now he would probably say">
-            <meta name="description" content="chien">
+			<meta name="title" content="carbonlollipop.com">
+            <meta name="description" content="thoughtbook, links, and fun!">
 		</head>
 		<body>
 			<main>
-				<div class="centered">
-					<h1>carbonlollipop.com</h1>
-					<img height="200" src="trolley.png" id="trolley"></img>
+				<div class="row">
+					<div class="column">
+						<h2 style="text-decoration: underline; cursor: pointer;">Links</h2>
+						<nav>
+							<a href="https://www.youtube.com/channel/UCxjeKhATcfrhZGTgy_J-NgA">YouTube</a>
+							<br>
+							<br>
+							<a href="https://anilist.co/user/Carbon/">AniList</a>
+							<br>
+							<br>
+							<a href="https://namemc.com/CarbonLollipop">NameMC</a>
+							<br>
+							<br>
+							<a href="https://www.reddit.com/u/CarbonLollipop">Reddit</a>
+							<br>
+							<br>
+							<a href="https://twitter.com/CarbonLollipop">Twitter</a>
+							<br>
+							<br>
+							<a href="https://github.com/CarbonLollipop">Github</a>
+							<br>
+							<br>
+							<p> <span style="text-decoration: underline; cursor: pointer;" onclick="navigator.clipboard.writeText('CarbonLollipop#9880');"> CarbonLollipop#9880 </span><br> <small>click to copy to clipboard</small></p>
+						</nav>
+					</div>
+					<div class="column">
+						<h2>Thoughtbook</h2>
+						<small>coming "soon"™</small>
+					</div>
 				</div>
-				<small id="note">placeholder</small>
+				<small id="note">no longer placeholder!   <a href="todos.php">todos</a></small>
 			</main>
 		</body>
 	</html>
