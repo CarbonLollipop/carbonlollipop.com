debfb12616360b5aa410f319e884cc86fe531c1b
CarbonLollipop <93494083+CarbonLollipop@users.noreply.github.com>
2022-12-15 20:19:37 -0800

    Add files via upload

---
+++ todos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>todos</title>
+</head>
+<body>
+    <a href="/">take me home</a>
+    <h1>in. order!!!</h1>
+    <ol>
+        <li>cute little icons next to links</li>
+        <li>thoughtbook (works exactly like the one on georgiy.world (but embedded into the root page!11!!!!!!!!))</li>
+        <li>demessify code (its a messy messy messy disgusting mess right now)</li>
+        <li>somehow get $10 so i can keep this running once my free trial of hosting runs out</li>
+        <li>text copied notification</li>
+        <li>favorite color (like the old version of this site except stored in a cookie)</li>
+        <li>maybe accounts if i can actually pay for hosting</li>
+        <li>ama</li>
+        <li>viewcount</li>
+    </ol>
+</body>
+</html>
